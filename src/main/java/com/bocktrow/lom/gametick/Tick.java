f7ef46cab3d10304d7b656779a2f6de1df2d5602
Bocktrow <spyrospagk@gmail.com>
2015-11-23 20:01:48 +0200

    Adding features

--- src/main/java/com/bocktrow/lom/gametick/Tick.java
+++ src/main/java/com/bocktrow/lom/gametick/Tick.java
@@ -18,7 +18,7 @@ public class Tick {
     public static void tick() {
         GamePlayer.getGamePlayers().values().forEach(gamePlayer -> {
             AbilityVisualizer.visualize(gamePlayer);
-            if (currentTick % 2 == 0) ScoreboardVisualizer.visualize(gamePlayer);
+            if (currentTick % 4 == 0) ScoreboardVisualizer.visualize(gamePlayer);
         });
 
         currentTick++;
