68212bfa74565879936526eeeed4e514078ea314
Bocktrow <spyrospagk@gmail.com>
2015-11-25 19:13:19 +0200

    Changes are too many to log...

--- src/main/java/com/bocktrow/lom/building/tower/Tower.java
+++ src/main/java/com/bocktrow/lom/building/tower/Tower.java
@@ -40,7 +40,7 @@ public class Tower extends Building {
 
                     Vector vectorInit = vector.clone();
 
-                    entity.damage(10);
+                    entity.damage(8);
 
                     for (int i = 0; i <= 50; i++) {
                         ParticleEffect.FLAME.display(0F, 0F, 0F, 0, 3, location.subtract(vector), 20);
